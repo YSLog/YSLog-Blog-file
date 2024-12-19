1cd4628a74ff958425a4c828da6690cb256885ee
YSLog <shanlongyuan3@gmail.com>
2024-12-19 17:52:28 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,9 @@
-# 构建你的博客
-hugo
+hugo #构造你的blog
 
-# 初始化 Git 仓库（如果尚未初始化）
-git init
+git init #初始化git
 
-# 添加所有生成的文件
 git add -A
 
-# 提交更改
 git commit -m 'deploy'
 
-# 强制推送到 GitHub Pages 仓库的 main 分支
-git push -f git@github.com:YSLog/YSLog.github.io.git main
+git push -f git@github.com:YSLog/YSLog.github.io.git main#向存储库推送
