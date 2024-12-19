6711e541a6a7e12334e2b0f603ac6221cfc0300e
YSLog <shanlongyuan3@gmail.com>
2024-12-19 19:13:05 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,8 @@ git add -A
 
 git commit -m 'deploy'
 
-git push -f git@github.com:YSLog/YSLog-Blog-file.git main#向存储库推送
+git push -f git@github.com:YSLog/YSLog-blog-file.git main
 
 ./deploy.sh > deploy.log 2>&1
+
 tail -n 20 deploy.log  # 查看最后20行日志
